c6ed9601429bb78fd2761314c592ffa048bfc57c
wendu24 <704506365@qq.com>
2025-10-05 09:47:47 +0800

    统计基础类

---
+++ credit-business/src/main/java/com/ruoyi/business/domain/StatHourDO.java
@@ -0,0 +1,79 @@
+package com.ruoyi.business.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@TableName("sh_stat_hour")
+public class StatHourDO {
+
+    /**
+     * 主键ID
+     */
+    private Long id;
+
+    /**
+     * 社区ID
+     */
+    private Long communityId;
+
+    /**
+     * 社区名称
+     */
+    private String communityName;
+
+    /**
+     * 统计日期（格式：YYYY-MM-DD）
+     */
+    private String statDay;
+
+    /**
+     * 统计小时（格式：HH）
+     */
+    private String statHour;
+
+    /**
+     * 平均温度
+     */
+    private BigDecimal avgTemperature;
+
+    /**
+     * 平均湿度
+     */
+    private BigDecimal avgHumidity;
+
+    /**
+     * 平均回水温度
+     */
+    private BigDecimal avgReturnWaterTemperature;
+
+    /**
+     * 平均供水温度
+     */
+    private BigDecimal avgSupplyWaterTemperature;
+
+    /**
+     * 平均供水压力
+     */
+    private BigDecimal avgSupplyWaterPressure;
+
+    /**
+     * 平均回水压力
+     */
+    private BigDecimal avgReturnWaterPressure;
+
+    /**
+     * 平均阀门开度
+     */
+    private BigDecimal avgValvePosition;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+
+}
---
+++ credit-business/src/main/java/com/ruoyi/business/service/impl/StatHourServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ruoyi.business.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ruoyi.business.domain.StatHourDO;
+import com.ruoyi.business.mapper.StatHourMapper;
+import com.ruoyi.business.service.StatHourService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+public class StatHourServiceImpl extends ServiceImpl<StatHourMapper, StatHourDO> implements StatHourService {
+
+
+
+
+
+}
